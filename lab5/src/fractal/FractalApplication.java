3ce3cdf3527dd3099904b0a712f0421cb117d416
andreasalmgren <andreas.almgren16@gmail.com>
2022-02-21 20:53:56 +0100

    D4

--- lab5/src/fractal/FractalApplication.java
+++ lab5/src/fractal/FractalApplication.java
@@ -7,11 +7,11 @@ import mountain.Point;
 public class FractalApplication {
 	public static void main(String[] args) {
 		Fractal[] fractals = new Fractal[2];
-		fractals[0] = new Mountain(new Point(100,300),
-				                   new Point(400,200),
-				                   new Point(550,420),
-				           15);
-		fractals[1] = new Koch(300);
-	    new FractalView(fractals, "Fraktaler", 600, 600);
+		fractals[0] = new Mountain(new Point(100, 300),
+				new Point(400, 200),
+				new Point(550, 420),
+				15);
+		fractals[1] = new Koch(300);	
+		new FractalView(fractals, "Fraktaler", 600, 600);
 	}
 }
